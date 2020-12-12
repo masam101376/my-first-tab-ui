ffa30eaba8e92d0281a6dbc5a5d50c62f70b9bb4
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-12 13:14:11 +0900

    Listen to nav-item's click event.

---
+++ tab.js
@@ -0,0 +1,30 @@
+// 即時関数: 気付かない間にグローバル変数を汚染しないために命令を囲っておくもの。js書くときはとりあえず囲っておく！
+(()=>{
+    // ここに命令を書く
+    const $doc = document;
+    const $tab = $doc.getElementById("js-tab");
+    const $nav = $tab.querySelectorAll('[data-nav]');
+    const $content = $tab.querySelectorAll('[data-content]');
+
+    // jsで最初に実行させたい命令を書く関数init
+    const init = () => {
+        $content[0].style.display = 'block';
+    };
+    init();
+
+    // クリックしたら怒るイベント
+    const handleClick = (e) => {
+        e.preventDefault();
+        // 前提: clickHandlerはクリックを検知するためのもの
+        // preventDefault();: もともと設置されていたクリックイベントに対するリアクションを無効化するためのもの。
+
+        console.log('clicked!');
+    };
+
+    let index = 0;
+    while(index < $nav.length){
+        $nav[index].addEventListener('click', (e) => handleClick(e));
+        index++;
+    }
+
+})();
